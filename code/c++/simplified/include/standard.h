2c0e9afe0bf3a3ff81b4a4b9eaf2c6a2939719d0
PotatoMindPopper <jeppe.vonk@hotmail.com>
2022-11-28 16:28:10 +0100

    creating clearscreen

--- code/c++/simplified/include/standard.h
+++ code/c++/simplified/include/standard.h
@@ -1,4 +1,6 @@
 #ifndef __STANDARD_H__
 #define __STANDARD_H__
 
+int clearScreen(void);
+
 #endif // __STANDARD_H__
--- code/c++/simplified/src/standard.cc
+++ code/c++/simplified/src/standard.cc
@@ -1,1 +1,21 @@
 #include "standard.h"
+#include <iostream>
+
+int clearScreen(void) {
+    int success = -1;
+
+    #if defined(_WIN32) || defined(_WIN64)
+        success = system("cls");
+    #else
+        success = system("clear");
+    #endif
+
+    if (success != 0) {
+        std::cerr << "Error: Failed to clear the screen." << std::endl;
+        std::cout << "Press enter to try again." << std::endl;
+        std::cin.get();
+        std::cout << "\033[2J\033[1;1H";
+    }
+
+    return 0;
+}
