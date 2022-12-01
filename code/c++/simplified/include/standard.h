50878a41deef99d2be233a1c46c90ec1692e33cf
PotatoMindPopper <jeppe.vonk@hotmail.com>
2022-12-01 10:34:23 +0100

    Add basic functions and constants

--- code/c++/simplified/include/standard.h
+++ code/c++/simplified/include/standard.h
@@ -2,5 +2,9 @@
 #define __STANDARD_H__
 
 void clearScreen();
+void enterContinue();
+
+template <typename T>
+T rng(const T &min, const T &max);
 
 #endif // __STANDARD_H__
--- code/c++/simplified/src/standard.cc
+++ code/c++/simplified/src/standard.cc
@@ -1,19 +1,34 @@
 #include "standard.h"
 #include <iostream>
+#include <random>
 
 void clearScreen() {
     int success = -1;
 
-    #if defined(_WIN32) || defined(_WIN64)
-        success = system("cls");
-    #else
-        success = system("clear");
-    #endif
+#if defined(_WIN32) || defined(_WIN64)
+    success = system("cls");
+#else
+    success = system("clear");
+#endif
 
     if (success != 0) {
         std::cerr << "Error: Failed to clear the screen." << std::endl;
         std::cout << "Press enter to try again." << std::endl;
-        std::cin.get();
+        // std::cin.get();
+        std::cin.ignore();
         std::cout << "\033[2J\033[1;1H";
     }
+}
+
+void enterContinue() {
+    std::cout << "Press enter to continue." << std::endl;
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
+
+template <typename T>
+T rng(const T &min, const T &max) {
+    static std::random_device rd;
+    static std::mt19937 rng(rd());
+    std::uniform_int_distribution<T> dist(min, max);
+    return dist(rng);
 }
