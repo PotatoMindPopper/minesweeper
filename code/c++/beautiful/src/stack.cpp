6bf680392c52bca391fb4f07a5197beb07bff12f
PotatoMindPopper <potatomindpopper@outlook.com>
2023-02-01 19:45:24 +0100

    Remove, for current time, not needed files

--- code/c++/beautiful/src/stack.cpp
+++
@@ -1,8 +0,0 @@
-// #include "stack.hpp"
-// #include "standard.hpp"
-// #include <iostream>
-
-// /**
-//  * @brief A menu for the game.
-//  */
-// void Stack::menu() { std::cout << "Stack Menu" << std::endl; }
